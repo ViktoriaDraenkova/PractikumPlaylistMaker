a98bece2ce694b990c00751a3087ed7ddc9901d7
Victoria <vikadraenkova@yandex.by>
2024-01-17 16:29:12 +0300

    first attempt sprint 10

--- app/src/main/java/com/practicum/appplaylistmaker/MusicViewHolder.kt
+++ app/src/main/java/com/practicum/appplaylistmaker/MusicViewHolder.kt
@@ -1,18 +1,23 @@
 package com.practicum.recyclerview_lesson_1
 
+import android.content.Context
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 import com.practicum.appplaylistmaker.R
 import com.practicum.appplaylistmaker.Track
+
+
 
 class MusicViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private val soundAuthor: TextView
     private val soundName: TextView
     private val soundDuration: TextView
-    private val soundImage:ImageView
+    private val soundImage: ImageView
 
     init {
         soundAuthor = itemView.findViewById(R.id.soundAuthor)
@@ -22,10 +27,18 @@ class MusicViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     }
 
     fun bind(model: Track) {
-        soundAuthor.text = model.artistName
-        soundName.text= model.trackName
-        soundDuration.text=model.trackTime
+        val requestOptions =
+            RequestOptions().transform(RoundedCorners(2.dpToPx(itemView.context))) // Скругление углов радиусом 2dp
 
-        Glide.with(itemView).load(model.artworkUrl100).error(R.drawable.page_not_found_background).into(soundImage)
+        soundAuthor.text = model.artistName
+        soundName.text = model.trackName
+        soundDuration.text = model.trackTime
+
+        Glide.with(itemView).load(model.artworkUrl100).error(R.drawable.placeholder)
+            .apply(requestOptions).into(soundImage)
+    }
+    fun Int.dpToPx(context: Context): Int {
+        val density = context.resources.displayMetrics.density
+        return (this * density).toInt()
     }
 }
