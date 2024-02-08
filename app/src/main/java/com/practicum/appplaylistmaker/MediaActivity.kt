91220d58b65bc0477d7a184323d3b2fc2a0c4c9f
Victoria <vikadraenkova@yandex.by>
2024-02-08 22:54:16 +0300

    some cleanup

--- app/src/main/java/com/practicum/appplaylistmaker/MediaActivity.kt
+++ app/src/main/java/com/practicum/appplaylistmaker/MediaActivity.kt
@@ -1,14 +1,12 @@
 package com.practicum.appplaylistmaker
 
-import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 
 class MediaActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setContentView(R.layout.activity_media)
     }
 }
